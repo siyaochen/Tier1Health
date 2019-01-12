06262ecf50b3ffd9e4b0a09eb33b766b37f1e830
Siyao Chen <siyaochen416@gmail.com>
2019-01-12 16:17:25 -0500

    Add Twilio test

---
+++ send_sms.py
@@ -0,0 +1,13 @@
+import os
+from twilio.rest import Client
+
+account_sid = os.environ["TWILIO_ACCOUNT_SID"]
+auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+
+client = Client(account_sid, auth_token)
+
+client.messages.create(
+    to = os.environ["MY_PHONE_NUMBER"],
+    from_ = "+16476967454",
+    body = "Twilio test message!"
+)
