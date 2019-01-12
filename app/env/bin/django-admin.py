4192481d9ec8342b6fbd5ec52ec56f9ebc030073
Kevin <victorliao18@gmail.com>
2019-01-12 16:18:26 -0500

    django initialized

---
+++ app/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/victorliao/Documents/sprojects/starterhacks/Tier1Health/app/env/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
